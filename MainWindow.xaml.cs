b16d04ce3f2b7ee7f64124f8914e0c2223516154
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-11-27 11:34:09 +0100

    book listing and viewing works

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,7 +19,38 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        var data = Data.books;
-        return;
+        var load = Data.books;
+    }
+
+    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var booklist = (ListBox) FindName("BooksListBox")!;
+        foreach (var book in Data.books)
+        {
+            var listitem = new ListBoxItem();
+            listitem.Content = book.Title;
+            listitem.Tag = book;
+            listitem.Selected += Item_Click;
+            booklist.Items.Add(listitem);
+        }
+    }
+
+    private void Item_Click(object sender, RoutedEventArgs e)
+    {
+        var title = FindName("TitleLabel") as Label;
+        var author = FindName("AuthorLabel") as Label;
+        var genre = FindName("GenreLabel") as Label;
+        var status =  FindName("StatusLabel") as Label;
+        var totalpages = FindName("TotalPagesLabel") as Label;
+        var currentpage = FindName("CurrentPageLabel") as Label;
+        var item = sender as ListBoxItem;
+        var book = item!.Tag as Book;
+        title.Content = book.Title;
+        author.Content = book.Author;
+        genre.Content = book.Genre;
+        status.Content = book.Status;
+        totalpages.Content = book.TotalPages.ToString();
+        currentpage.Content = book.CurrentPage.ToString();
+
     }
 }
