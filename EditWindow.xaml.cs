b68d996f27a8cb8c93b15b5afea95463e61c5577
VargaDaniel2007 <varga.daniel@students.jedlik.eu>
2025-11-30 15:41:00 +0100

    Final touches on EditWindow: Data Saving, Data checking, Error messages, Closing Window on cancel

--- EditWindow.xaml.cs
+++ EditWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using static System.Reflection.Metadata.BlobBuilder;
 
 namespace WPFbooks;
@@ -24,6 +28,7 @@ public partial class EditWindow : Window
 
         txtTitle.Text = book.Title;
         txtAuthor.Text = book.Author;
+        txtPublishYear.Text = Convert.ToString(book.PublishedYear);
 
         LoadGenres();
         cbGenre.SelectedItem = book.Genre.Name;
@@ -71,29 +76,99 @@ public partial class EditWindow : Window
 
     private void btnCancel_Click(object sender, RoutedEventArgs e)
     {
-
+        //Return to MainWindow
     }
 
     private void btnSave_Click(object sender, RoutedEventArgs e)
     {
-        if (Data.books.Count(book => book.Id == bookId) == 0)
-            NewBook();
+        if (Data.books.Count(book => book.Id == bookId) != 0)
+        {
+            ModifyBookData(Data.books.First(book => book.Id == bookId));
 
+        }
         else
-            ModifyBook(Data.books.First(book => book.Id == bookId));
+        {
+            Book newBook = new Book();
+            Data.books.Add(newBook);
+            ModifyBookData(newBook);
+        }
 
     }
 
-    private void ModifyBook(Book book)
+    private void ModifyBookData(Book book)
     {
-        if(txtAuthor.Text != "" && txtTitle.Text != "" && Data.genres.Any(genre => genre.Name == cbGenre.Text) )    //Number TryParse check
-        {
+        List<Control> errors = new List<Control>();
+        int publishYear = -1;
+        int currentPage = -1;
+        int totalPage = -1;
+        if (txtAuthor.Text == "")
+            errors.Add(txtAuthor);
+        if (txtTitle.Text == "")
+            errors.Add(txtTitle);
 
+        if (!int.TryParse(txtPublishYear.Text, out publishYear))
+            errors.Add(txtPublishYear);
+
+        if (!Data.genres.Any(genre => genre.Name == cbGenre.Text))
+            errors.Add(cbGenre);
+
+        if (!int.TryParse(txtCurrentPage.Text, out currentPage) || currentPage < 0)
+            errors.Add(txtCurrentPage);
+        if (!int.TryParse(txtTotalPages.Text, out totalPage) || totalPage < 0)
+            errors.Add(txtTotalPages);
+
+        if(currentPage > totalPage)
+        {
+            errors.Add(txtCurrentPage);
+        }
+        
+        if(errors.Count > 0)
+        {
+            ModifyBookError(errors);
+        }
+        else
+        {
+            book.Author = txtAuthor.Text;
+            book.Title = txtTitle.Text;
+            book.PublishedYear = publishYear;
+            book.Genre = Data.genres.First(genre => genre.Name == cbGenre.Text);
+
+            if (rbNotStarted.IsChecked == true)
+                book.Status = (BookStatus)0;
+            else if (rbStarted.IsChecked == true)
+                book.Status = (BookStatus)1;
+            else
+                book.Status = (BookStatus)2;
+
+            book.CurrentPage = currentPage;
+            book.TotalPages = totalPage;
+
+            //Data.SaveData();      ---------> AFTER TESTING UNCOMMENT
         }
     }
 
-    private void NewBook()
+    private void ModifyBookError(List<Control> errors)
     {
-        throw new NotImplementedException();
+        foreach(Control e in errors)
+        {
+            getErrorLabel(e).Visibility = Visibility.Visible;
+            e.BorderBrush = Brushes.Red;
+        }
+    }
+
+    private void txt_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var errorLbl = getErrorLabel((sender as Control)!);
+
+        if (errorLbl.Visibility == Visibility.Visible)
+        {
+            errorLbl.Visibility = Visibility.Hidden;
+            (sender as Control)!.ClearValue(BorderBrushProperty);
+        }
+    }
+
+    private Label getErrorLabel(Control control)
+    {
+        return (FindName(control.Tag as String) as Label)!;
     }
 }
